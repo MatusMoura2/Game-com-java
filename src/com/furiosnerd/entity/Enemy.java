6655d288791a1397c4f5878a973712d6c5b0bd3c
Matus Moura <matus.moura333x2@gmail.com>
2024-04-01 22:00:43 -0300

    sistema de IA dos inimigos

--- src/com/furiosnerd/entity/Enemy.java
+++ src/com/furiosnerd/entity/Enemy.java
@@ -2,11 +2,29 @@ package com.furiosnerd.entity;
 
 import java.awt.image.BufferedImage;
 
-public class Enemy extends Entity{
+import com.furiosnerd.main.Game;
+import com.furiosnerd.map.Map;
+
+public class Enemy extends Entity {
+
+	private double speed = 0.5;
 
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
-		
+	}
+
+	public void spin() {
+		if (x < Game.player.getX() && Map.isFree((int) (x + speed), this.getY())) {
+			x += speed;
+		} else if (x > Game.player.getX() && Map.isFree((int) (x - speed), this.getY())) {
+			x -= speed;
+		}
+
+		if (y < Game.player.getY() && Map.isFree(this.getY(), (int) (y + speed))) {
+			y += speed;
+		} else if (y > Game.player.getY() && Map.isFree(this.getY(), (int)(y-speed))) {
+			y -= speed;
+		}
 	}
 
 }
