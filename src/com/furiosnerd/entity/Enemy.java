afbe77c0ae3dc85f57001c842a213faacfa9b1f1
Matus Moura <matus.moura333x2@gmail.com>
2024-04-04 20:19:33 -0300

    Animação dos inimigos

--- src/com/furiosnerd/entity/Enemy.java
+++ src/com/furiosnerd/entity/Enemy.java
@@ -1,6 +1,5 @@
 package com.furiosnerd.entity;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
@@ -13,9 +12,16 @@ public class Enemy extends Entity {
 
 	private double speed = 0.5;
 	private int maskx = 8, masky = 8, maskw = 6, maskh = 6;
+	private int frames = 0, maxFrames = 20, index = 0, maxIndex = 2;
+
+	private BufferedImage[] spritesEnemy;
 
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
-		super(x, y, width, height, sprite);
+		super(x, y, width, height, null);
+		spritesEnemy = new BufferedImage[3];
+		spritesEnemy[0] = Game.spritesheet.getSprite(112, 0, 16, 16);
+		spritesEnemy[1] = Game.spritesheet.getSprite(128, 0, 16, 16);
+		spritesEnemy[2] = Game.spritesheet.getSprite(144, 0, 16, 16);
 	}
 
 	public void spin() {
@@ -33,6 +39,15 @@ public class Enemy extends Entity {
 		} else if ((int) y > Game.player.getY() && Map.isFree(this.getX(), (int) (y - speed))
 				&& !isColliding(this.getX(), (int) (y - speed))) {
 			y -= speed;
+		}
+
+		frames++;
+		if (frames == maxFrames) {
+			frames = 0;
+			index++;
+			if (index > maxIndex) {
+				index = 0;
+			}
 		}
 	}
 
@@ -52,11 +67,16 @@ public class Enemy extends Entity {
 		return false;
 	}
 
-	//para ver a dimanção do
+	/**
+	 * para ver a dimanção da mascara de colisão basta tirar o comentario desse
+	 * metodo.
+	 */
 	public void render(Graphics g) {
-		super.render(g);
-		g.setColor(Color.BLUE);
-		g.fillRect(this.getX() + maskx - Camera.x, this.getY() + masky - Camera.y, maskw, maskh);
+		g.drawImage(spritesEnemy[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+		/*
+		 * g.setColor(Color.BLUE); g.fillRect(this.getX() + maskx - Camera.x,
+		 * this.getY() + masky - Camera.y, maskw, maskh);
+		 */
 	}
 
 }
